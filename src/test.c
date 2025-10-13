30586135610062fae943d5da204d1e779f5c6835
nonk123 <me@nonk.dev>
2025-10-13 15:28:47 +0300

    Clean up nutpunchTest

--- src/test.c
+++ src/test.c
@@ -18,6 +18,119 @@ static const int nameCount = sizeof(randomNames) / sizeof(*randomNames);
 static struct Player players[NUTPUNCH_MAX_PLAYERS] = {0};
 #define PAYLOAD_SIZE ((size_t)(2))
 
+uint8_t waitingForPlayers, _waitingForPlayers;
+static void maybe_start_netgame() {
+	if (!waitingForPlayers || NutPunch_LocalPeer() == NUTPUNCH_MAX_PLAYERS)
+		return;
+	int size = 0, *ptr = NutPunch_LobbyGet("PLAYERS", &size);
+	if (size != 1 || !*ptr || NutPunch_PeerCount() < *ptr)
+		return;
+	memset(players, 0, sizeof(players));
+	players[NutPunch_LocalPeer()].x = poor_width() / 2;
+	players[NutPunch_LocalPeer()].y = poor_height() / 2;
+	waitingForPlayers = 0;
+}
+
+static void maybe_join_netgame() {
+	if (poor_key_pressed(POOR_J))
+		NutPunch_Join(lobbyName);
+	else if (poor_key_pressed(POOR_H))
+		NutPunch_Host(lobbyName);
+	else
+		return;
+
+	waitingForPlayers = _waitingForPlayers;
+	NutPunch_LobbySet(magicKey, sizeof(magicValue), &magicValue);
+	NutPunch_LobbySet("PLAYERS", 1, &waitingForPlayers);
+
+	const char* name = randomNames[rand() % nameCount];
+	NutPunch_PeerSet("NAME", (int)strlen(name) + 1, name);
+}
+
+static void draw_players() {
+	for (int i = 0; i < NUTPUNCH_MAX_PLAYERS; i++) {
+		const int x = players[i].x, y = players[i].y;
+		if (NutPunch_LocalPeer() == i) {
+			poor_at(x, y)->chr = ' ';
+			poor_at(x, y)->bg = POOR_GREEN;
+		} else if (NutPunch_PeerAlive(i)) {
+			poor_at(x, y)->chr = ' ';
+			poor_at(x, y)->bg = POOR_RED;
+		}
+	}
+}
+
+static void receive_shit() {
+	static uint8_t data[512] = {0};
+	while (NutPunch_HasMessage()) {
+		int size = sizeof(data), peer = NutPunch_NextMessage(data, &size);
+		if (peer == NUTPUNCH_MAX_PLAYERS)
+			continue;
+		if (size == PAYLOAD_SIZE) {
+			players[peer].x = ((int32_t)(data[0]));
+			players[peer].y = ((int32_t)(data[1]));
+		} else
+			printf("%s\n", data);
+	}
+}
+
+static void chat_with(int idx) {
+	char name1[32] = {0}, name2[32] = {0};
+
+	const char* theirName = NutPunch_PeerGet(idx, "NAME", NULL);
+	if (theirName == NULL)
+		return;
+	snprintf(name1, sizeof(name1), "%s", theirName);
+
+	const char* ourName = NutPunch_PeerGet(NutPunch_LocalPeer(), "NAME", NULL);
+	if (ourName == NULL)
+		return;
+	snprintf(name2, sizeof(name2), "%s", ourName);
+
+	static char buf[96] = {0};
+	snprintf(buf, sizeof(buf), "[%s]: Hi, %s!", name2, name1);
+
+	NutPunch_SendReliably(idx, buf, sizeof(buf));
+}
+
+static void send_shit() {
+	static uint8_t data[512] = {0};
+	if (NutPunch_LocalPeer() == NUTPUNCH_MAX_PLAYERS)
+		return;
+
+	data[0] = (uint8_t)(players[NutPunch_LocalPeer()].x);
+	data[1] = (uint8_t)(players[NutPunch_LocalPeer()].y);
+
+	for (int i = 0; i < NUTPUNCH_MAX_PLAYERS; i++) {
+		NutPunch_Send(i, data, PAYLOAD_SIZE);
+		if (poor_key_pressed(POOR_T))
+			chat_with(i);
+	}
+}
+
+static void move_our_dot() {
+	if (NutPunch_LocalPeer() == NUTPUNCH_MAX_PLAYERS)
+		return;
+	players[NutPunch_LocalPeer()].x += poor_key_down(POOR_D) - poor_key_down(POOR_A);
+	players[NutPunch_LocalPeer()].y += poor_key_down(POOR_S) - poor_key_down(POOR_W);
+}
+
+static void draw_debug_bits(int status) {
+	const int chrs[] = {
+		'D',
+		'B',
+		'G',
+		'0' + (NPS_Online == status),
+		'0' + NutPunch_IsMaster(),
+		'0' + waitingForPlayers,
+		'0' + NutPunch_PeerCount(),
+	};
+
+	const int x = 0, y = 0;
+	for (int i = 0; i < sizeof(chrs) / sizeof(*chrs); i++)
+		poor_at(x + i, y)->chr = chrs[i];
+}
+
 int main(int argc, char* argv[]) {
 	if (argc != 2 && argc != 3) {
 		printf("YOU FIALED ME!!!! NOW SUFFERRRRR\n");
@@ -26,7 +139,7 @@ int main(int argc, char* argv[]) {
 	if (argc == 3)
 		NutPunch_SetServerAddr(argv[2]);
 
-	int waitingForPlayers = strtol(argv[1], NULL, 10), _waitingForPlayers = waitingForPlayers;
+	waitingForPlayers = strtol(argv[1], NULL, 10), _waitingForPlayers = waitingForPlayers;
 	if (waitingForPlayers < 2) {
 		printf("The fuck do you mean?\n");
 		return EXIT_FAILURE;
@@ -38,91 +151,20 @@ int main(int argc, char* argv[]) {
 		if (poor_key_pressed(POOR_ESC) || poor_key_pressed(POOR_Q))
 			poor_exit();
 
-		int status = NutPunch_Update(), statusX = 0;
-		poor_at(statusX++, 0)->chr = 'F';
-		poor_at(statusX++, 0)->chr = '0' + (NPS_Online == status);
-		poor_at(statusX++, 0)->chr = '0' + NutPunch_IsMaster();
+		int status = NutPunch_Update();
+		draw_debug_bits(status);
+		maybe_start_netgame();
 
-		static uint8_t data[512] = {0};
-		while (NutPunch_HasMessage()) {
-			int size = sizeof(data), peer = NutPunch_NextMessage(data, &size);
-			if (peer == NUTPUNCH_MAX_PLAYERS)
-				continue;
-			if (size == PAYLOAD_SIZE) {
-				players[peer].x = ((int32_t)(data[0]));
-				players[peer].y = ((int32_t)(data[1]));
-			} else
-				printf("%s\n", data);
-		}
+		receive_shit();
+		move_our_dot();
+		send_shit();
 
-		if (waitingForPlayers && NutPunch_LocalPeer() != NUTPUNCH_MAX_PLAYERS) {
-			int size = 0, *ptr = NutPunch_LobbyGet("PLAYERS", &size);
-			if (sizeof(int) == size && *ptr && NutPunch_PeerCount() >= *ptr) {
-				memset(players, 0, sizeof(players));
-				players[NutPunch_LocalPeer()].x = poor_width() / 2;
-				players[NutPunch_LocalPeer()].y = poor_height() / 2;
-				waitingForPlayers = 0;
-			}
-		}
-
-		if (NutPunch_LocalPeer() != NUTPUNCH_MAX_PLAYERS) {
-			players[NutPunch_LocalPeer()].x += poor_key_down(POOR_D) - poor_key_down(POOR_A);
-			players[NutPunch_LocalPeer()].y += poor_key_down(POOR_S) - poor_key_down(POOR_W);
-		}
-
-		data[0] = (uint8_t)(players[NutPunch_LocalPeer()].x);
-		data[1] = (uint8_t)(players[NutPunch_LocalPeer()].y);
-		for (int i = 0; i < NUTPUNCH_MAX_PLAYERS; i++) {
-			NutPunch_Send(i, data, PAYLOAD_SIZE);
-			char name1[32] = {0}, name2[32] = {0};
-
-			const char* theirName = NutPunch_PeerGet(i, "NAME", NULL);
-			if (theirName == NULL)
-				continue;
-			snprintf(name1, sizeof(name1), "%s", theirName);
-
-			const char* ourName = NutPunch_PeerGet(NutPunch_LocalPeer(), "NAME", NULL);
-			if (ourName == NULL)
-				continue;
-			snprintf(name2, sizeof(name2), "%s", ourName);
-
-			static char buf[96] = {0};
-			snprintf(buf, sizeof(buf), "[%s]: Hi, %s!", name2, name1);
-			if (poor_key_pressed(POOR_T))
-				NutPunch_SendReliably(i, buf, sizeof(buf));
-		}
-
-		if (NPS_Error == status) {
+		if (NPS_Error == status)
 			NP_Log("ERROR: %s", NutPunch_GetLastError());
-			continue;
-		} else if (NPS_Online == status) {
-			for (int i = 0; i < NUTPUNCH_MAX_PLAYERS; i++) {
-				const int x = players[i].x, y = players[i].y;
-				if (NutPunch_LocalPeer() == i) {
-					poor_at(x, y)->chr = ' ';
-					poor_at(x, y)->bg = POOR_GREEN;
-				} else if (NutPunch_PeerAlive(i)) {
-					poor_at(x, y)->chr = ' ';
-					poor_at(x, y)->bg = POOR_RED;
-				}
-			}
-			continue;
-		}
-
-		waitingForPlayers = _waitingForPlayers;
-		if (poor_key_pressed(POOR_J))
-			NutPunch_Join(lobbyName);
-		else if (poor_key_pressed(POOR_H))
-			NutPunch_Host(lobbyName);
+		else if (NPS_Online == status)
+			draw_players();
 		else
-			goto skip_network;
-
-		NutPunch_LobbySet(magicKey, sizeof(magicValue), &magicValue);
-		NutPunch_LobbySet("PLAYERS", sizeof(waitingForPlayers), &waitingForPlayers);
-		const char* name = randomNames[rand() % nameCount];
-		NutPunch_PeerSet("NAME", (int)strlen(name) + 1, name);
-
-	skip_network:
+			maybe_join_netgame();
 		if (poor_key_pressed(POOR_K))
 			NutPunch_Reset();
 	}
